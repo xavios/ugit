74431288c4489adb0fcca6abf9bf622ceece7462
Andras Acs <mr.acs.andras@gmail.com>
2021-10-02 21:27:49 +0200

    cat-file works

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 from . import data
 
@@ -20,7 +21,11 @@ def parse_args():
 
     hash_object_parser = commands.add_parser('hash-object')
     hash_object_parser.set_defaults(func=hash_object)
-    hash_object_parser.add_argument("file")
+    hash_object_parser.add_argument('file')
+
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('oid')
 
     return parser.parse_args()
 
@@ -34,3 +39,8 @@ def init(args):
 def hash_object(args):
     with open(args.file, 'rb') as f:
         print(data.hash_object(f.read()))
+
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.oid))
--- ugit/data.py
+++ ugit/data.py
@@ -14,3 +14,8 @@ def hash_object(data):
     with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
         out.write(data)
     return oid
+
+
+def get_object(oid):
+    with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+        return f.read()
