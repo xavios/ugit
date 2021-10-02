bd7780ca18584aa77bdb630a1d100ad7bf9eb22f
Andras Acs <mr.acs.andras@gmail.com>
2021-10-02 21:38:39 +0200

    add type to saved objects

--- ugit/data.py
+++ ugit/data.py
@@ -2,6 +2,7 @@ import os
 import hashlib
 
 GIT_DIR = '.ugit'
+TYPE_CONTENT_SEPARATOR = b'\x00'
 
 
 def init():
@@ -9,13 +10,19 @@ def init():
     os.makedirs(f'{GIT_DIR}/objects')
 
 
-def hash_object(data):
-    oid = hashlib.sha1(data).hexdigest()
+def hash_object(data, type_='blob'):
+    object = type_.encode() + TYPE_CONTENT_SEPARATOR + data
+    oid = hashlib.sha1(object).hexdigest()
     with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
-        out.write(data)
+        out.write(object)
     return oid
 
 
-def get_object(oid):
+def get_object(oid, expected='blob'):
     with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
-        return f.read()
+        object = f.read()
+    type_, _, content = object.partition(TYPE_CONTENT_SEPARATOR)
+    type_ = type_.decode()
+    if expected is not None:
+        assert type_ == expected, f'Expected {expected}, got {type_}'
+    return content
