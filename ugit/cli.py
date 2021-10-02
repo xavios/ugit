c94111d6be26e222430905f1e840eb21bc71157a
Andras Acs <mr.acs.andras@gmail.com>
2021-10-02 21:19:02 +0200

    save object accessible by content hash

--- ugit/cli.py
+++ ugit/cli.py
@@ -18,6 +18,10 @@ def parse_args():
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
 
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument("file")
+
     return parser.parse_args()
 
 
@@ -25,3 +29,8 @@ def init(args):
     data.init()
     print(
         f'Initialized empty ugit repository in {os.path.join(os.getcwd(), data.GIT_DIR)}')
+
+
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
